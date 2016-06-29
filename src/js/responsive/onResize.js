b2cf6fc447620fa9093aa4cf72577f80566bec66
max-kernix <mchevasson@kernix.com>
2016-06-29 17:21:52 +0200

    Add: Js on resize tweak skeleton

---
+++ src/js/responsive/onResize.js
@@ -0,0 +1,20 @@
+//// Manage on reisze events & functions calls
+
+/// Imports
+// Import lodash.throttle
+// var _throttle = require('lodash/throttle');
+
+
+// doc.ready
+
+  // Functions initial calls, when docment ready
+    // allResizeFunctions()
+    // when done > no FOUC proc
+
+  // Max 0.2s spam on resize event
+  // jQuery(window).on('resize', _throttle(function(true) { // true > calls when ends // check syntax
+    // allResizeFunctions()
+
+  // }, 200));
+
+// allResizeFunctions()
